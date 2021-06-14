69aa1b87fe21258f14001651a976b8f6641ceab8
Rishabh Chatterjee <nanya.login@protonmail.com>
2021-06-14 13:31:33 +0530

    Configure linting

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 // markup
-const IndexPage = () => <h1>Hello from Gatsby!</h1>;
+const IndexPage = () => {
+  return <h1>Hello from Gatsby!</h1>;
+};
 
 export default IndexPage;
